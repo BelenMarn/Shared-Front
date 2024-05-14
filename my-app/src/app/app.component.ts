74bdfcdacc7e1f3c0c593e1e0f743043ee12208c
Belen Marin <marinsanchezbelen@gmail.com>
2024-05-14 16:31:02 +0200

    Routes done

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { MenuComponent } from './core/components/menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Shared-Front';
 }
